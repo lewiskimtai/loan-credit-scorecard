7a6f6e99b10b6d0e000735f4023ac1ad3fc84fac
lewiskimtai <lewiskimtai@gmail.com>
2023-08-13 20:25:08 +0300

    streamlit

--- streamlit.py
+++ streamlit.py
@@ -8,7 +8,7 @@ import streamlit.components.v1 as components
 import requests
 
 # Dataset
-url = "phone_df.csv"
+url = "Loan.xlsx"
 
 # Prediction Function connected to the FAST API
 def get_predictions(CHK_ACCT, DURATION,	HISTORY, SAV_ACCT, EMPLOYMENT, REAL_ESTATE,	PROP_UNKN_NONE,	AGE, OTHER_INSTALL,	OWN_RES):
@@ -21,11 +21,11 @@ def get_predictions(CHK_ACCT, DURATION,	HISTORY, SAV_ACCT, EMPLOYMENT, REAL_ESTA
 
 # Side Bar
 # st.sidebar.image('photos/logo.png')
-# st.sidebar.header("Phone Price Prediction")
+st.sidebar.header("Loan Credit Score")
 menu = ['Get Score','Exploratory Data Analysis','About']
 selection = st.sidebar.selectbox("", menu)
 
-st.sidebar.write('Predicting Loan Score Credits for new customers is important to Financial Institutions to predict customers probability to pay the loan and avoid costs that come with bad loan customers.')
+st.sidebar.write('Financial institutions find it crucial to forecast the likelihood of loan repayment for new clients in order to anticipate the potential for loan default and mitigate the expenses associated with borrowers who may not fulfill their repayment obligations.')
 
 # Get Price Functionality
 if selection == 'Get Score':
@@ -34,73 +34,60 @@ if selection == 'Get Score':
     st.title('Enter customer details')
     # st.image('photos/specs.png')
     CHK_ACCT = st.number_input('Customer account status.', min_value=0 , max_value=3)
-    st.write('0 (account status with < 0 DM) 1 (account status with 0 < ...< 200 DM) 2 (account status with => 200 DM) 3 (no checking account)') 
+    st.write('0 (account status with < 0 DM), 1 (account status with 0 < ...< 200 DM), 2 (account status with => 200 DM), 3 (no checking account)') 
     DURATION = st.number_input('Customer duration of credit in months.')
-    HISTORY = st.number_input('Customer credit history. \n0 for no credits taken, 1 for all credits at this bank paid back duly, 2 for existing credits paid back duly till now, 3 for delay in paying off in the past, 4 for critical account', min_value=0 , max_value=4)
-    SAV_ACCT = st.number_input('Customer average balance in savings account. \n0 for acc avg bal < 100 DM, 1 for acc avg bal 100<= ... <  500 DM, 2 for acc avg bal 500<= ... < 1000 DM, 3 for acc avg bal =>1000 DM, 4 for unknown/ no savings account', min_value=0 , max_value=4)
-    EMPLOYMENT = st.number_input('Customer employment period. \n0 for unemployed, 1 for < 1 yr, 2 for 1 <= ... < 4 yrs, 3 for 4 <=... < 7 yrs, 4 for >= 7 yrs', min_value=0 , max_value=4)
-    REAL_ESTATE = st.number_input('Customer owns real estate. \n0 for No, 1 for Yes', min_value=0 , max_value=1)
-    PROP_UNKN_NONE = st.number_input('Customer owns no property (or unknown). \n0 for No, 1 for Yes', min_value=0 , max_value=1)
+    HISTORY = st.number_input('Customer credit history.', min_value=0 , max_value=4)
+    st.write('0 (no credits taken), 1 (all credits at this bank paid back duly), 2 (existing credits paid back duly till now), 3 (delay in paying off in the past), 4 (critical account)')
+    SAV_ACCT = st.number_input('Customer average balance in savings account.', min_value=0 , max_value=4)
+    st.write('0 (acc avg bal < 100 DM), 1 (acc avg bal 100<= ... <  500 DM), 2 (acc avg bal 500<= ... < 1000 DM), 3 (acc avg bal =>1000 DM), 4 (unknown/ no savings account)')
+    EMPLOYMENT = st.number_input('Customer employment period.', min_value=0 , max_value=4)
+    st.write('0 (unemployed), 1 (< 1 yr), 2 (1 <= ... < 4 yrs), 3 (4 <=... < 7 yrs), 4 (>= 7 yrs)')
+    REAL_ESTATE = st.number_input('Customer owns real estate.', min_value=0 , max_value=1)
+    st.write('0 - No, 1 - Yes')
+    PROP_UNKN_NONE = st.number_input('Customer owns no property (or unknown).', min_value=0 , max_value=1)
+    st.write('0 - No, 1 - Yes')
     AGE = st.number_input('Customers age in years.')
-    OTHER_INSTALL = st.number_input('Customer has other installment plan credit. \n0 for No, 1 for Yes', min_value=0 , max_value=1)
-    OWN_RES = st.number_input('Customer owns residence. \n0 for No, 1 for Yes', min_value=0 , max_value=1)
+    OTHER_INSTALL = st.number_input('Customer has other installment plan credit.', min_value=0 , max_value=1)
+    st.write('0 - No, 1 - Yes')
+    OWN_RES = st.number_input('Customer owns residence.', min_value=0 , max_value=1)
+    st.write('0 - No, 1 - Yes')
     result = ""
 
     # when 'Predict' is clicked, make the prediction and store it
     if st.button("Predict"):
-        result = int(np.exp(get_predictions(brand=brand, screen_size=screen_size, ram=ram, rom=rom, mp=mp, battery=battery)))
-        st.success(f'Price of Phone: {result} UGX')
+        result = int(np.exp(get_predictions(CHK_ACCT=CHK_ACCT, DURATION=DURATION, HISTORY=HISTORY, SAV_ACCT=SAV_ACCT, EMPLOYMENT=EMPLOYMENT, REAL_ESTATE=REAL_ESTATE, PROP_UNKN_NONE=PROP_UNKN_NONE, AGE=AGE, OTHER_INSTALL=OTHER_INSTALL, OWN_RES=OWN_RES)))
+        st.success(f'Credit Rating: {result}')
+        st.write('0 - No, 1 - Yes')
         # st.image('photos/phoness.jpg')
 
 
         
 # Exploratory Data Analysis Functionality
 if selection == 'Exploratory Data Analysis':    
-    phone_df = pd.read_csv(url, encoding="ISO-8859-1", low_memory=False)
+    df = pd.read_excel(url, encoding="ISO-8859-1", low_memory=False)
     st.markdown('Display data')
-    st.table(phone_df.head())
+    st.table(df.head())
 
-    # scatter plot of brand and price
-    st.header("Brand and Price")
-    fig = px.scatter(phone_df, x='brand', y='price', opacity = 0.25, template = 'plotly_dark', 
-                    color='brand', hover_data=['brand', 'price'],title = 'PHONE PRICE PREDICTION (BRAND - PRICE RELATIONSHIP)')
-    fig.update_layout(width=900, height=500)
-    st.plotly_chart(fig)
+    # Total Amount Disbursed
+    # Calculate the sum of 'AMOUNT' column
+    total_amount = df['AMOUNT'].sum()
+    # Print the sum with commas as thousands separators
+    st.write('{total_amount}')
 
-    # scatter plot of screen_size and price
-    st.header("Screen size and Price")
-    fig = px.scatter(phone_df, x='screen_size', y='price', opacity = 0.25, template = 'plotly_dark', 
-                    color='screen_size', hover_data=['screen_size', 'price'],title = 'PHONE PRICE PREDICTION (SCREEN SIZE - PRICE RELATIONSHIP)')
-    fig.update_layout(width=900, height=500)
-    st.plotly_chart(fig)
+    # Percentage of Good and Bad Creditors
+    # Calculate value counts
+    response_counts = df['RESPONSE'].value_counts()
+    # Create the pie chart using Plotly Express
+    fig = px.pie(response_counts, names=response_counts.index, values=response_counts.values, title='PIE CHART SHOWING GOOD & BAD DEBTORS')
+    # Display the pie chart using Streamlit
+    st.plotly_chart(fig, use_container_width=True)
 
-    # scatter plot of ram and price
-    st.header("RAM and Price")
-    fig = px.scatter(phone_df, x='ram', y='price', opacity = 0.25, template = 'plotly_dark', 
-                    color='ram', hover_data=['ram', 'price'],title = 'PHONE PRICE PREDICTION (RAM - PRICE RELATIONSHIP)')
-    fig.update_layout(width=900, height=500)
-    st.plotly_chart(fig)
+    # Amount Disbursed in relation to Response
+    response_amount_sum = df.groupby('RESPONSE')['AMOUNT'].sum()
+    # Print the total amount for each response with commas
+    print(response_amount_sum.apply('{:,}'.format))
+    st.write('{response_amount_sum}')
 
-    # scatter plot of rom and price
-    st.header("ROM and Price")
-    fig = px.scatter(phone_df, x='rom', y='price', opacity = 0.25, template = 'plotly_dark', 
-                    color='rom', hover_data=['rom', 'price'],title = 'PHONE PRICE PREDICTION (ROM - PRICE RELATIONSHIP)')
-    fig.update_layout(width=900, height=500)
-    st.plotly_chart(fig)
-
-    # scatter plot of mp and price
-    st.header("MP and Price")
-    fig = px.scatter(phone_df, x='mp', y='price', opacity = 0.25, template = 'plotly_dark', 
-                    color='mp', hover_data=['mp', 'price'],title = 'PHONE PRICE PREDICTION (MEGA PIXELS - PRICE RELATIONSHIP)')
-    fig.update_layout(width=900, height=500)
-    st.plotly_chart(fig)
-
-    # scatter plot of battery and price
-    st.header("Battery and Price")
-    fig = px.scatter(phone_df, x='battery', y='price', opacity = 0.25, template = 'plotly_dark', 
-                    color='battery', hover_data=['battery', 'price'],title = 'PHONE PRICE PREDICTION (BATTERY - PRICE RELATIONSHIP)')
-    fig.update_layout(width=900, height=500)
-    st.plotly_chart(fig)
 
 # adding html  Template
 
@@ -114,7 +101,7 @@ footer_temp = """
 	    <div class="container" id="aboutapp">
 	      <div class="row">
 	        <div class="col l6 s12">
-	          <h5 class="white-text">Phone Price Prediction</h5>
+	          <h5 class="white-text">Loan Credit Score</h5>
 	          <p class="grey-text text-lighten-4">Streamlit Practical.</p>
 	        </div>
 	   <div class="col l3 s12">
@@ -144,10 +131,10 @@ footer_temp = """
 if selection == 'About':
     st.subheader("About App")
     st.text('''
-                Finding a dream phone at a friendly price without compromising on the ideal features seems impossible, 
-                but with this app it makes it possible. This app is built on data about different phone brands, 
-                features and their respective prices.
-                Trained to predict your favorite phone price according to its specs and brand making it easy for you 
-                to afford your number one dream phone.
+                Welcome to our Credit Score App, your reliable partner in assessing and understanding your financial health. 
+            Our user-friendly platform empowers you with insights into your creditworthiness, offering a comprehensive analysis 
+            of your credit history and providing valuable guidance to help you make informed financial decisions. 
+            Whether you're planning for a major purchase, seeking better loan terms, or simply aiming to enhance your financial well-being, 
+            our Credit Score App is here to support you on your journey towards a secure and prosperous future.
             ''')
     components.html(footer_temp, height=500)
